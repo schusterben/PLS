ea35451a373a3ca1c795f0a7b63d858e3b5fdbec
Masty3 <113447817+Masty3@users.noreply.github.com>
2023-11-13 23:35:28 +0100

    PersonsTableSeeder so angepasst (koordinaten um Sbg)

--- database/seeders/PersonsTableSeeder.php
+++ database/seeders/PersonsTableSeeder.php
@@ -27,8 +27,8 @@ class PersonsTableSeeder extends Seeder
                 'dringend' => rand(0, 1), // Zufälliger Wert (0 oder 1) für "dringend"
                 'kontaminiert' => rand(0, 1), // Zufälliger Wert (0 oder 1) für "kontaminiert"
                 'name' => 'Patient' . ($i + 1), // Name basierend auf dem Index
-                'longitude_patient' => rand(-180, 180) + (rand(0, 999999) / 1000000), // Zufällige Longitude im Bereich von -180 bis 180
-                'latitude_patient' => rand(-90, 90) + (rand(0, 999999) / 1000000),    // Zufällige Latitude im Bereich von -90 bis 90
+                'longitude_patient' => mt_rand(13060, 13090) / 1000, // Zufällige Longitude im Bereich von -180 bis 180
+                'latitude_patient' => mt_rand(47930, 47940) / 1000,    // Zufällige Latitude im Bereich von -90 bis 90
                 'user_iduser' => rand(1, 10), // Zufällige Benutzer-ID (angepasst an deine Benutzerdaten)
                 'created_at' => now(),       // Aktuelles Datum und Uhrzeit
                 'updated_at' => now(),       // Aktuelles Datum und Uhrzeit
