1037b2e32e475b6171ffd8cd26e1d4c3c04c639b
markozovkic <200943@fhwn.ac.at>
2023-11-23 19:00:11 +0100

    User Migration angepasst Beim erstellen wird gleich ein User angelegt.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
+
 
 return new class extends Migration
 {
@@ -13,12 +16,24 @@ return new class extends Migration
     {
         Schema::create('user', function (Blueprint $table) {
             $table->id('iduser');
+            $table->string('username')->nullable();
+            $table->string('password')->nullable();
+            $table->boolean('adminRole');
             $table->float('longitude_user', 10, 6)->nullable();
             $table->float('latitude_user', 10, 6)->nullable();
             $table->dateTime('first_login_time')->nullable();
             $table->dateTime('last_login_time')->nullable();
             $table->timestamps();
         });
+
+
+
+        DB::table('user')->insert([
+            'username' => 'admin',
+            'password' => Hash::make('admin'), // Hier solltest du das Passwort richtig hashen
+            'adminRole' => true,
+
+        ]);
     }
 
     /**
