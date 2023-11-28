002fe5e8c7a3b7f2da697b3b05a92491624c45d2
markozovkic <200943@fhwn.ac.at>
2023-11-28 09:17:59 +0100

    patient

---
+++ database/migrations/2023_11_05_120002_create_patient_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('patient', function (Blueprint $table) {
+            $table->id('idpatient');
+            $table->tinyInteger('atmung');
+            $table->tinyInteger('blutung');
+            $table->string('radialispuls');
+            $table->string('triagefarbe');
+            $table->tinyInteger('transport');
+            $table->tinyInteger('dringend');
+            $table->tinyInteger('kontaminiert');
+            $table->string('name')->nullable();
+            $table->float('longitude_patient', 10, 6)->nullable();
+            $table->float('latitude_patient', 10, 6)->nullable();
+            $table->unsignedBigInteger('user_iduser');
+            $table->foreign('user_iduser')->references('iduser')->on('user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patient');
+    }
+};
