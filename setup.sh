1d7c3317170760816761644c2f7785e65ee270be
208918-MG <208918@fhwn.ac.at>
2025-11-11 17:48:30 +0100

    Update setup.sh
    Updated Setup.sh equal to Main Branche

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -e
 
 # Repository klonen
 echo "Cloning repository..."
-git clone https://github.com/schusterben/PLS.git --branch dev_sage
+git clone https://github.com/schusterben/PLS.git
 cd PLS
 
 # Composer-Abhängigkeiten installieren
@@ -16,15 +16,27 @@ docker run --rm -v $(pwd):/app composer:latest composer require tymon/jwt-auth
 
 # Node.js-Abhängigkeiten installieren
 echo "Installing Node.js dependencies..."
-docker run --rm -v $(pwd):/app -w /app node:18 npm install
+sudo docker run --rm -v $(pwd):/app -w /app node:18 npm install
 docker run --rm -v $(pwd):/app -w /app node:18 npm install vite --save-dev
+
+# Laravel Docker Image lokal erstellen
+echo "Building Laravel Docker image..."
+docker build -t pls-laravel_app:latest .
 
 # Docker-Container starten
 echo "Starting Docker containers..."
 docker-compose up -d --build
 
+# Warten bis Container hochfährt
+echo "Warte bis Container hochfährt"
+sleep 10
+
 # Datenbankmigrationen ausführen
 echo "Running database migrations..."
-docker-compose exec laravel_app php artisan migrate
+sudo docker-compose exec laravel_app php artisan migrate
+
+sudo chmod -R 775 storage bootstrap/cache
+sudo chown -R www-data:www-data storage bootstrap/cache
+ 
 
 echo "Setup complete! Your application is now running."
